f5aeab6c66747856539c9e5a884b691b18065abf
adwoa71 <kpugach@mail.ru>
2022-12-14 06:42:43 +0300

    rename .mjs to .js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import greetingYou  from '../src/cli.mjs';
+import greetingYou  from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
 console.log (greetingYou());
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,6 @@
 import readlineSync from 'readline-sync';
-
 const greetingYou = () => {
-	let userName = readlineSync.question('May I have your name? ');
+	var userName = readlineSync.question('May I have your name? ');
 	return 'Hello,' + userName + '!';
 };
-
-
 export default greetingYou;
