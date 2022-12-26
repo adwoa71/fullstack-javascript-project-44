bae4d22092829cbac90963e35b245cad2a44630e
adwoa71 <kpugach@mail.ru>
2022-12-26 14:27:02 +0300

    remove Welcome to ../src/cli.js from brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
 import greetingYou  from '../src/cli.js';
-console.log('Welcome to the Brain Games!');
 console.log (greetingYou());
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 const greetingYou = () => {
+	console.log ("Welcome to the Brain Games!")
 	var userName = readlineSync.question('May I have your name? ');
 	return 'Hello,' + userName + '!';
 };
